aced2d896ef773218fde81bbec79546782478db1
Jesus <jegico93@gmail.com>
2020-07-27 01:27:56 +0200

    Nueva bienvenida

--- index.js
+++ index.js
@@ -49,30 +49,37 @@ client.on('guildMemberAdd', async member => {
 	const background = await Canvas.loadImage('./img/bg.png');
 	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-	ctx.strokeStyle = '#74037b';
+	// ctx.shadowColor = '#d53';
+	// ctx.shadowBlur = 20;
+	ctx.lineJoin = 'bevel';
+	ctx.lineWidth = 10;
+	ctx.strokeStyle = '#fff';
 	ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
 	// Slightly smaller text placed above the member's display name
 	ctx.font = '28px sans-serif';
 	ctx.fillStyle = '#ffffff';
-	ctx.fillText('Bienvenido al servidor,', canvas.width / 2.5, canvas.height / 3.5);
+	ctx.shadowColor = "rgba(0,0,0,0.9)";
+	ctx.shadowBlur = 10;
+	ctx.fillText(`Hola ${member.user.tag}!`, canvas.width / 2.7, canvas.height / 2.5);
 
 	// Add an exclamation point here and below
-	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.font = applyText(canvas, 'Bienvenid@');
 	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-
+	ctx.fillText(`Bienvenid@`, canvas.width / 2.7, canvas.height / 1.5);
+	ctx.shadowBlur = 0;
 	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+	ctx.arc(125, 125, 75, 0, Math.PI * 2, true);
+	ctx.stroke();
 	ctx.closePath();
 	ctx.clip();
 
 	const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-	ctx.drawImage(avatar, 25, 25, 200, 200);
+	ctx.drawImage(avatar, 50, 50, 150, 150);
 
 	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 
-	channel.send(`${member} acaba de llegar!`, attachment);
+	channel.send(`${member} acaba de llegar a Pecado City! 👋`, attachment);
 });
 
 const applyText = (canvas, text) => {
