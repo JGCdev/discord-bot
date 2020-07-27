8106a6bc24c1e458683b84508e24616addf04c91
Jesus <jegico93@gmail.com>
2020-07-28 00:27:30 +0200

    Añadido pin a comando IP

--- commands/ip.js
+++ commands/ip.js
@@ -4,7 +4,7 @@ module.exports = {
 	execute(message, args) {
 		message.channel.send({embed: {
 			color: 3447003,
-			title: "IP del servidor",
+			title: "📌 IP del servidor",
 			fields: [{
 				name: "234.34.56.12:1200",
 				value: "Para acceder abre FiveM, pulsa F8 e introduce la IP"
